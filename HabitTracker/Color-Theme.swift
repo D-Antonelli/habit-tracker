71915a9478becff7360435140a31afb07cf4c2b5
derya <derya@boonandmoil.dev>
2022-07-28 15:04:07 +0100

    adds colour theme

---
+++ HabitTracker/Color-Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Color-Theme.swift
+//  HabitTracker
+//
+//  Created by Derya Antonelli on 28/07/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.85, green: 0.85, blue: 0.85)
+    }
+}
