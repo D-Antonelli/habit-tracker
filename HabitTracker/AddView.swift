b997da8ea42828b52988116c534bdba82bab0f08
derya <derya@boonandmoil.dev>
2022-08-04 12:27:33 +0100

    adds and styles text views

--- HabitTracker/AddView.swift
+++ HabitTracker/AddView.swift
@@ -11,37 +11,81 @@ import SwiftUI
 struct AddView: View {
     @ObservedObject var habits: Habits
     
+    @State private var habitName: String = ""
+    
     var body: some View {
         Group {
-            VStack {
+            VStack(spacing: 50) {
                 HStack {
                     Text("CREATE")
-                        .font(.largeTitle)
+                        .font(.title)
                     
                     Spacer()
-                    
                     HStack(spacing: 30) {
                         Button {
                             // save habit
                         } label: {
                             Text("SAVE")
                                 .padding(12)
+                                .foregroundColor(.black)
                                 .background(.white)
                                 .modifier(WhiteRoundedRectangleShape())
-                                
+                            
                         }
                         
                         Button {
                             // exit screen
                         } label: {
-                            Image(systemName: "plus")
+                            Image(systemName: "multiply")
+                                .font(.title2.bold())
+                                .foregroundColor(.black)
                         }
                     }
                 }
+                
+                List {
+                    Group {
+                        Section {
+                            TextField("Coffee, tea, jogging, etc.", text: $habitName)
+                                .padding()
+                                .background(.white)
+                                .modifier(WhiteRoundedRectangleShape())
+                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                                
+                            
+                        } header: {
+                            Text("NAME YOUR HABIT")
+                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                                .foregroundColor(.black)
+                                .font(.body)
+                        }
+                        
+                        
+                        Section {
+                            TextField("Coffee, tea, jogging, etc.", text: $habitName)
+                                .padding()
+                                .background(.white)
+                                .modifier(WhiteRoundedRectangleShape())
+                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                                
+                            
+                        } header: {
+                            Text("DEFAULT COUNT")
+                                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                                .foregroundColor(.black)
+                                .font(.body)
+                        }
+                        
+                    }
+                    .listRowBackground(Color.clear)
+                    .listRowSeparator(.hidden)
+                    
+                }
+                .listStyle(PlainListStyle())
+                
             }
-            
         }
-        .padding()
+        .padding(25)
         .background(.darkBackground)
     }
     
