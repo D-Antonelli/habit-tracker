486af80a0f00489a2d5f3316a46a138185a29aef
derya <derya@boonandmoil.dev>
2022-09-15 12:45:58 +0100

    converts styling into default

--- HabitTracker/AddView.swift
+++ HabitTracker/AddView.swift
@@ -9,10 +9,6 @@ import SwiftUI
 
 
 struct AddView: View {
-    private enum Field: Int, CaseIterable {
-        case habit, count, note
-    }
-    
     @ObservedObject var habits: Habits
     
     @State private var habitName: String = ""
@@ -21,103 +17,46 @@ struct AddView: View {
     
     @Environment(\.presentationMode) var presentationMode
     
-    @FocusState private var focusedField: Field?
-    
     
     var body: some View {
-        Group {
-            VStack {
                 HStack {
                     Text("CREATE")
-                        .font(.title)
-                    
                     Spacer()
-                    HStack(spacing: 30) {
                         Button {
-                            focusedField = nil
                             habits.add(habit:Habit(name: habitName, count: count, note: note))
                             habits.save()
                             presentationMode.wrappedValue.dismiss()
                         } label: {
                             Text("SAVE")
-                                .padding(EdgeInsets(top: 8, leading: 10, bottom: 8, trailing: 10))
-                                .foregroundColor(.black)
-                                .background(.white)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(.white))
                         }
                         
                         Button {
                             presentationMode.wrappedValue.dismiss()
                         } label: {
                             Image(systemName: "multiply")
-                                .font(.title2.bold())
-                                .foregroundColor(.black)
                         }
                     }
-                }
-                
-                List {
-                    Group {
-                        Section {
-                            TextField("Coffee, tea, jogging, etc.", text: $habitName)
-                                .padding()
-                                .background(.white)
-                                .modifier(WhiteRoundedRectangleShape())
-                                .focused($focusedField, equals: .habit)
-                                
-                            
-                        } header: {
-                            Text("NAME YOUR HABIT:")
-                                .foregroundColor(.black)
-                              
-                        }
-                        
-                        
-                        Section {
-                            Stepper("\(count)", value: $count, in: 1...9999999)
-                                .padding()
-                                .background(.white)
-                                .modifier(WhiteRoundedRectangleShape())
-                                .focused($focusedField, equals: .count)
-                                .frame(maxWidth: 200)
-                                
-                            
-                        } header: {
-                            Text("DEFAULT COUNT:")
-                                .foregroundColor(.black)
-                            
-                        }
-                        
-                        
-                        Section {
-                            TextEditor(text: $note)
-                                .padding()
-                                .background(.white)
-                                .modifier(WhiteRoundedRectangleShape())
-                                .frame(height: 150)
-                                .focused($focusedField, equals: .note)
-                            
-                        } header: {
-                            Text("NOTES:")
-                                .foregroundColor(.black)
-                            
-                        }
-                        
-                    }
-                    .listRowBackground(Color.clear)
-                    .listRowSeparator(.hidden)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                    
-                }
-               
-                .listStyle(PlainListStyle())
-                
-            }
+                .padding()
+                .background(.red)
 
-        }
-        .padding()
-        .background(.darkBackground)
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text("NAME YOUR HABIT:")
+                        TextField("Coffee, tea, jogging, etc.", text: $habitName)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("DEFAULT COUNT")
+                        Stepper("\(count)", value: $count, in: 1...9999999)
+                    }
+                    
+                    VStack(alignment: .leading) {
+                        Text("NOTES:")
+                        TextEditor(text: $note)
+                            .background(.blue)
+                            .frame(height: 150)
+                    }
+                }
     }
     
     
--- HabitTracker/DetailView.swift
+++ HabitTracker/DetailView.swift
@@ -13,82 +13,49 @@ struct DetailView: View {
     
     @State private var count: Int = 1
     @State private var note: String = ""
-
+    
     
     var body: some View {
-        List {
-            VStack( alignment: .leading, spacing: 30) {
-                VStack(alignment: .leading) {
-                    Text("TOTAL COUNT")
-                        .bold()
-                    Stepper("\(count) Times", value: $count, in: 1...9999)
-                        .frame(maxWidth: 200)
-                        .padding()
-                        .background(.white)
-                        .modifier(WhiteRoundedRectangleShape())
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("TOTAL COUNT")
+                Stepper("\(count) Times", value: $count, in: 1...9999)
+            }
+            
+            VStack(alignment: .leading) {
+                Text("NOTES")
+                TextEditor(text: $note)
+                    .frame(height: 150)
+            }
+            
+            HStack {
+                Button {
+                    habits.update(from: self.habit, to: Habit(name: habit.name, count: count, note: note))
+                    habits.save()
+                } label: {
+                    Text("SAVE")
                 }
                 
-                VStack(alignment: .leading) {
-                    Text("NOTES")
-                        .bold()
-                    TextEditor(text: $note)
-                        .padding()
-                        .background(.white)
-                        .modifier(WhiteRoundedRectangleShape())
-                        .frame(height: 150)
-                }
-                
-                
-                HStack {
-                    Button {
-                        habits.update(from: self.habit, to: Habit(name: habit.name, count: count, note: note))
-                        habits.save()
-                    } label: {
-                        Text("SAVE")
-                            .padding(EdgeInsets(top: 9, leading: 18, bottom: 9, trailing: 18))
-                            .background(.green)
-                            .foregroundColor(.white)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
-                    
-                    Button {
-                        habits.delete(habit: habit)
-                        habits.save()
-                    } label: {
-                        Text("DELETE")
-                            .padding(EdgeInsets(top: 9, leading: 18, bottom: 9, trailing: 18))
-                            .background(.red)
-                            .foregroundColor(.white)
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
+                Button {
+                    habits.delete(habit: habit)
+                    habits.save()
+                } label: {
+                    Text("DELETE")
                 }
             }
-            .listRowBackground(Color.clear)
-            .listRowSeparator(.hidden)
-            
-            
         }
-        .onAppear() {
-            if let i = habits.list.firstIndex(where: { $0.id == self.habit.id }) {
-                self.count = habits.list[i].count
-                self.note = habits.list[i].note
-            }
-        }
-        .padding([.vertical])
-        .listStyle(.plain)
-        .buttonStyle(.borderless)
-        
-        .navigationTitle(habit.name)
-        .background(.darkBackground)
-    }
-        
-        
-        
-        
-}
-
-struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView(habits: Habits(), habit: Habit(name: "Drink water", count: 3, note: "2bottles/day"))
+                .onAppear() {
+                    if let i = habits.list.firstIndex(where: { $0.id == self.habit.id }) {
+                        self.count = habits.list[i].count
+                        self.note = habits.list[i].note
+                    }
+                }
+                .navigationTitle(habit.name)
     }
 }
+    
+    struct DetailView_Previews: PreviewProvider {
+        static var previews: some View {
+            DetailView(habits: Habits(), habit: Habit(name: "Drink water", count: 3, note: "2bottles/day"))
+        }
+    }
